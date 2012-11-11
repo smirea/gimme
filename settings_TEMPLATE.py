a6d858cf188d560c1381a63484ce62aa8d7345a3
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2012-11-11 01:45:12 +0000

    Update settings

--- settings_TEMPLATE.py
+++ settings_TEMPLATE.py
@@ -67,9 +67,10 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+  # Put strings here, like "/home/html/static" or "C:/www/django/static".
+  # Always use forward slashes, even on Windows.
+  # Don't forget to use absolute paths, not relative paths.
+  os.path.join(os.path.dirname(__file__), 'client'),
 )
 
 # List of finder classes that know how to find static files in
